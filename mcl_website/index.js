1b5325ec208416722bcc617c65dbedf70d71dd3e
tymkuc06 <52408060+tymgin@users.noreply.github.com>
2022-07-24 20:41:32 +0200

    Add files via upload

---
+++ mcl_website/index.js
@@ -0,0 +1,23 @@
+const about = document.getElementById("about")
+const home = document.getElementById("home")
+const contact = document.getElementById("contact")
+const news = document.getElementById("news")
+const aboutus = document.getElementById("aboutus")
+const descriptio = document.getElementById("descriptio")
+home.addEventListener('dbclick', function(){
+    home.textContent = "You clicked it too many times."
+aboutus.style.display = "none"
+})
+about.addEventListener('dbclick', function(){
+    about.textContent = "You clicked it too many times."
+})
+news.addEventListener('dblclick', function(){
+    about.textContent = "You clicked it too many times."
+})
+contact.addEventListener("dblclick", function(){
+    contact.textContent = You clicked it too many times
+})
+about.addEventListener("click", function(){
+    home.textContent = about
+    des
+})
